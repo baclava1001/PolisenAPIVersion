76cf39b5c3b977c9b8d6692259e3ad99c8edb677
Nujsk <111609565+Nujsk@users.noreply.github.com>
2024-04-03 18:20:35 +0200

    Add project files.

---
+++ BlazorAPIExperimental/Data/CrimeData.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorAPIExperimental.Data
+{
+
+    public class CrimeSummary
+    {
+        public int id { get; set; }
+        public string datetime { get; set; }
+        public string name { get; set; }
+        public string summary { get; set; }
+        public string url { get; set; }
+        public string type { get; set; }
+        public Location location { get; set; }
+    }
+
+    public class Location
+    {
+        public string name { get; set; }
+        public string gps { get; set; }
+    }
+
+}
---
+++ BlazorAPIExperimental/Data/Services.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+
+namespace BlazorAPIExperimental.Data
+{
+    public class Services
+    {
+        private readonly HttpClient _httpClient;
+        public Services(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<List<CrimeSummary>> GetData()
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync("?locationname=Umeå");
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadFromJsonAsync<List<CrimeSummary>>();
+            }
+            catch(HttpRequestException ex)
+            {
+                Console.WriteLine($"HTTP request failed: {ex.Message}");
+                return new List<CrimeSummary>();
+            }
+        }
+    }
+}
